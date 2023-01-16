7f644a52bf6942f4b94dc9567ed51a54f6536665
Roman <romeenice@gmail.com>
2023-01-16 12:19:42 +0200

    lodash.debounce

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { debounce } from 'lodash';
-
-import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const ref = {
+  inputRef: document.querySelector('#search-box'),
+  listRef: document.querySelector('.country-list'),
+  countryInfoRef: document.querySelector('.country-info'),
+};
+refs.inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  const inputValue = e.target.value.trim();
+  clearInput;
+}
