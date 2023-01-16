cf3c55fa6cc44aa58be15dc1b0f66cadff123f57
Roman <romeenice@gmail.com>
2023-01-16 16:49:32 +0200

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,7 +2,7 @@ const COUNTRY_URL = 'https://restcountries.com/v3.1/name/';
 
 export function fetchCountries(name) {
   return fetch(
-    `${COUNTRY_URL}${name}?fields=name,capital,popular,flags,languages`
+    `${COUNTRY_URL}${name}?fields=name,capital,population,flags,languages`
   ).then(response => {
     if (response.ok) {
       return response.json();
