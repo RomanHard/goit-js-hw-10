d817962cffa49e75b9d28f1e45d4faf311476f33
Roman <romeenice@gmail.com>
2023-01-16 12:34:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import { debounce } from 'lodash.debounce';
+import  debounce from 'lodash.debounce';
 import { fetchCountries } from './fetchCountries';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
 
-const ref = {
+
+const refs = {
   inputRef: document.querySelector('#search-box'),
   listRef: document.querySelector('.country-list'),
   countryInfoRef: document.querySelector('.country-info'),
 };
+
 refs.inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(e) {
   const inputValue = e.target.value.trim();
-  clearInput;
-}
+  clearInput();
+  if (!inputValue) {
+    return;
+  }
+  fetchCountries(inputValue)
+  .then(res => {
+    console.log(res);
+    const resLength = res.length;
+  })
