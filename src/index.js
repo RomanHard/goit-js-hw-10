d0914d398b27c7024cf5404c9e6b80e75678ece9
Roman <romeenice@gmail.com>
2023-01-16 13:36:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -27,10 +27,62 @@ function onInput(e) {
     if (resLength === 1) {
         const murkup = createOneMarup(res);
         refs.countryInfoRef.insertAdjacentHTML('beforeend', markup);
+        return;
     }
-  })
+    if (resLength > 10) {
+        Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+    }
+  }).catch(error => {
+    clearInput();
+    console.log(error);
+    Notify.failure('Oops, there is no country with that name')
+  });
 
-  function createOneMarup(countries =[]) {
+  function createOneCountryMarkup(countries = []) {
     return countries
-    .map()
+      .map(
+        country =>
+          `
+          <div class="country-info-wrapper">
+        <img class="country-info-flag" src="${country.flags.svg}"
+       alt="flag" width='30' hight='20'>
+       <h1 class="country-info-name">${country.name.official}</h1>
+       </div>
+       <ul class="country-list-info">
+       <li class="country-list-item">
+       <p class="country-list-signs">Capital: </p>
+       <span class="country-list-span">${country.capital}</span>
+       </li>
+       <li class="country-list-item">
+       <p class="country-list-signs">Population: </p>
+       <span class="country-list-span">${country.population}</span>
+       </li>
+       <li class="country-list-item">
+       <p class="country-list-signs">Languages: </p>
+       <span class="country-list-span">${Object.values(country.languages).join(
+         ', '
+       )}</span>
+       </li>
+       </ul>`
+      )
+      .join('');
+  }
+
+  function createCountriesList(countries = []) {
+    return countries
+      .map(
+        country => `<li class="country-list-item">
+    <img class="country-flag" src="${country.flags.svg}" alt="flag" width='30' hight='20'>
+    <p class="country-name">${country.name.official}</p>
+    </li>`
+      )
+      .join('');
+  }
+
+  function clearInput() {
+    refs.listRef.innerHTML = '';
+    refs.countryInfoRef.innerHTML = '';
   }
